ffbe6a50f84f8cd20dc463db0d0c0c25a41a2705
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-04-28 21:25:32 +0200

    RC8

--- app/db.js
+++ app/db.js
@@ -3,17 +3,19 @@ var mail = require('./mail');
 
 var pool = mysql.createPool({
     connectionLimit: 100, //important
-    host: 'localhost',
+    host: 'vgccrm.moj-servis.si',
     user: 'event',
-    password: 'event',
+    password: 'Vgccrm2=!/',
     database: 'live_events',
+    port: '20050',
     debug: false
 });
 
-const cntSql = 'SELECT count(*) cnt from register where sent = 0';
+const cntSql = 'SELECT count(*) cnt FROM register r, location l, event e ' +
+    'where r.sent = 0 and l.id = e.location_id and e.id = r.event_id and l.email is not null order by r.cdate desc;';
 const dataSql =
     'SELECT r.*,l.email lemail,e.name ename,e.starttime,e.endtime FROM register r, location l, event e ' +
-    'where r.sent = 0 and l.id = e.location_id and e.id = r.event_id order by r.cdate desc;';
+    'where r.sent = 0 and l.id = e.location_id and e.id = r.event_id and l.email is not null order by r.cdate desc;';
 const updateSql =
     'UPDATE register set sent = 1 where id =';
 
